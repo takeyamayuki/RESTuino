186d3ea932c44c5cc0ef61072007349c1ffc6a4f
Yuki Takeyama <namiki.takeyama@gmail.com>
2023-04-23 17:41:11 +0900

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
+#include <Arduino.h>
 #include <RESTuino.h>
 
 static RESTuino restuino;
 
 void setup()
 {
-  restuino.host_name = "restuino";
-  restuino.ssid_def = "";
-  restuino.ssid_pass = "";
-  restuino.setup();
+	restuino.host_name = "restuino";
+	restuino.ssid_def = "Extender-G-CE1C";
+	restuino.ssid_pass = "jifmckdmk6inr";
+	restuino.serial_baud = 9600;
+	restuino.setup();
 }
 void loop()
 {
-  restuino.loop();
+	restuino.loop();
 }
